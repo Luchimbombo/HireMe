27531b1cc5b57b87b71a885deac64ba0b7886b2f
AndresCastVill <35810023+AndresCastVill@users.noreply.github.com>
2018-01-26 13:34:19 -0300

    esto es una prueba

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php
+﻿<?php
 //Proceso de conexión con la base de datos
 $conex = mysql_connect("localhost", "root", "administrador")
-		or die("No se pudo realizar la conexion");
+		or die("No se pudo realizar la conexion con la bd");
 	mysql_select_db("basedatoshireme",$conex)
 		or die("ERROR con la base de datos");
 
