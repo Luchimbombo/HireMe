a30e7f3ce853f8acd4456b851bb0425cd776294b
AndresCastVill <35810023+AndresCastVill@users.noreply.github.com>
2018-01-29 23:53:42 -0300

    Agregué Ver perfil

--- jCrearPerfil.php
+++ jCrearPerfil.php
@@ -37,7 +37,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Jefe de Proyecto <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Ver Perfil</a></li>
+            <li><a href="jVerMiCuenta.php">Ver Perfil</a></li>
             <li><a href="logout.php">Salir</a></li>
           </ul>
         </li>
--- jFiltrarEspecialista.php
+++ jFiltrarEspecialista.php
@@ -65,7 +65,7 @@ if (!$_SESSION) {
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Jefe de Proyecto <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Ver Perfil</a></li>
+            <li><a href="jVerMiCuenta.php">Ver Perfil</a></li>
             <li><a href="logout.php">Salir</a></li>
           </ul>
         </li>
