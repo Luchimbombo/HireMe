e1608a88d5935aba9c0e8ff6a68465a4e7482077
AndresCastVill <35810023+AndresCastVill@users.noreply.github.com>
2018-02-01 14:20:19 -0300

    Agrega los trabajadores seleccionados a la tala solicitud

--- pruebaEnvio.php
+++ pruebaEnvio.php
@@ -1,9 +1,39 @@
 <?php
+session_start();
+$idpro=$_SESSION['idpro'];
+
+//Conexion BD
+
+$conex = mysql_connect("localhost", "root", "administrador") 
+or die("No se pudo realizar la conexion exitosamente");
+mysql_select_db("basedatoshireme", $conex) 
+or die("ERROR con la bd");
+
+	
+//Crear fecha actual
+
+date_default_timezone_set('Chile/Continental');
+$hoy=date_default_timezone_get();
+$fecha=getdate();
+$conv=array_merge((array)$fecha['year'],(array)$fecha['mon'], (array)$fecha['mday']);
+$fechaHoy = implode("-", $conv);
+
+//Bucle para recorrer lo enviado
 foreach($_POST['datos'] as $valor)
-{
-echo "<br>".$valor;
+{	
+
+$sql2="INSERT INTO solicitud SET fechaSolicitud='$fechaHoy',
+estadoSolicitud='1',
+IdProyecto='$idpro',
+idTrabajador='$valor'";
+mysql_query($sql2);
+
 }
 ?>
+<script language = javascript>
+	alert("Los datos han sido guardados y enviados exitosamente")
+	self.location = "eVerEspecialistaPrueba.php" 
+</script>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
